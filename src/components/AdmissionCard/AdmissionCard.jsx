a1ece1d8b5ee9c515582b21cb2715d4c961a50c4
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-07-23 18:01:46 +0600

    Admission cards added in the Admission route

---
+++ src/components/AdmissionCard/AdmissionCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AdmissionCard = ({college}) => {
+    const {
+        image,
+        name,
+      } = college;
+    return (
+        <div title='Click to Admission' className="flex  justify-start items-center space-x-3 hover:bg-[#e84766] hover:text-white duration-1000 p-2 mt-5 mr-2  rounded-2xl cursor-pointer">
+        <div className="avatar">
+          <div className="mask mask-squircle w-16 h-16 my-1">
+            <img src={image} alt="Avatar Tailwind CSS Component" />
+          </div>
+        </div>
+        <div>
+          <div className="font-bold text-lg">{name}</div>
+        </div>
+      </div>
+    );
+};
+
+export default AdmissionCard;
--- src/pages/Admission/Admission.jsx
+++ src/pages/Admission/Admission.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import img from '../../assets/images/admission-cover.jpg';
+import AdmissionCard from "../../components/AdmissionCard/AdmissionCard";
+import useColleges from "../../hooks/useColleges";
+import Cover from "../Shared/Cover/Cover";
 
 const Admission = () => {
+  const [colleges] = useColleges();
   return (
     <div className="lg:pt-28">
       <Helmet>
         <title>BDreams | Admission</title>
       </Helmet>
-      <h2>Admission</h2>
+      <Cover
+        bgImg={img}
+        title={"Take Admission"}
+        text={"Fill Up Your Dream Form!"}
+      ></Cover>
+      <div className="bg-slate-50 dark:bg-slate-700 grid grid-cols-1 lg:grid-cols-2 mx-2">
+        {colleges.map((college) => (
+          <AdmissionCard key={college._id} college={college}></AdmissionCard>
+        ))}
+      </div>
     </div>
   );
 };
